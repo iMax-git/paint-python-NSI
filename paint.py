a5c8f914c1c5714e6dfd37041fbce8b428e74ac4
iMax <45369969+iMax-git@users.noreply.github.com>
2020-09-09 14:40:07 +0200

    Selecteur de forme

--- paint.py
+++ paint.py
@@ -21,6 +21,7 @@ width = 0
 
 bg = 0
 fg = 0
+tx = 0
 
 def drawAll():
     global r, bg
@@ -41,7 +42,8 @@ def drawTools():
         canvas.create_rectangle(v1,0,v2,30,fill=c)
         v1 = v1 +30
         v2 = v2 + 30
-    canvas.create_rectangle(30*bg,0,30*bg+30,30,fill="", outline="red", width=2)
+    canvas.create_rectangle(30*bg,0,30*bg+30,30, outline="red", width=2)
+    canvas.create_rectangle(30*tx-2,0,30*tx+30-2,30,fill="darkgrey", outline="black", width=2)
     x = len(color)*30+5
     for i in range(1,5):
         if(i == width-1):
@@ -50,17 +52,17 @@ def drawTools():
             canvas.create_line(x+(10*i),0,x+(10*i),30, width=2*i)
     
     x = x+10*8
-    canvas.create_rectangle(x,0,x+30,30,fill="blue")
-    canvas.create_oval(x+31,0,x+61,30,fill="red")
-    canvas.create_oval(x+63,5,x+93,25,fill="green")
-    canvas.create_line(x+93,0,x+123,30,fill="green")
+    canvas.create_rectangle(x+5,0+4,x+30,30-5)
+    canvas.create_oval(x+35,2,x+60,25)
+    canvas.create_oval(x+65,5,x+90,25)
+    canvas.create_line(x+93,0,x+123,30)
 
 
 def create_circle(x,y,r,c,w,f):
     canvas.create_oval(x-r,y-r,x+r,y+r,fill=color[c], width=w, outline=color[f])
     
 def OnMousePress(event):
-    global startx, starty, bg, endx, endy, onDrag, width, fg
+    global startx, starty, bg, endx, endy, onDrag, width, fg, tx
     if(event.y <30 and event.x<30*len(color)):
         bg = event.x//30
     elif(event.y <30 and event.x<80+30*len(color)):
@@ -70,7 +72,7 @@ def OnMousePress(event):
             strock = event.x//10
         #print("strock" + str(strock))
     elif(event.y <30 and event.x<200+30*len(color)):
-        x = event.x - 30*4
+        tx = event.x // 30
         for i in range(1,5):
             tools = event.x//10
         print(tools)
